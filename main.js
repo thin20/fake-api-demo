6d7331e97c24748403cd102abad861d306efb27a
thin20 <70517637+thin20@users.noreply.github.com>
2021-06-18 09:50:54 +0700

    update PORT and deploy on heroku

--- main.js
+++ main.js
@@ -52,6 +52,7 @@ router.render = (req, res) => {
 
 // Use default router
 server.use('/api', router)
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
     console.log('JSON Server is running')
 })
